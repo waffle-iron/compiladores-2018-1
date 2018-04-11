fc84d1ab4cc26083340af4e193103d10f96e4e4b
Paulo Batista <pauloc@linuxmail.org>
2018-04-11 17:51:45 -0300

    function to identify call functions

--- syntax/Syntax.py
+++ syntax/Syntax.py
@@ -5,7 +5,8 @@ class syntax_analyzer(object):
         pass
 
 
-    def isNumber(self, token):
+    def isNumber(self, tokenlist):
+        token = tokenlist[0]
         if token.tokenval == TokenVal.INTEGER_NUMBER.value or
         token.tokenval == TokenVal.FLOAT_NUMBER.value or
         token.tokenval == TokenVal.SCIENTIFIC_NOTATION.value:
@@ -14,23 +15,24 @@ class syntax_analyzer(object):
             return False
 
 
-
-
-    def isSumOperator(self, token):
+    def isSumOperator(self, tokenlist):
+        token = tokenlist[0]
         if token.tokenval == TokenVal.PLUS.value or token.tokenval == TokenVal.MINUS.value:
             return True
         else:
             return False
 
 
-    def isMultiOperator(self, token):
+    def isMultiOperator(self, tokenlist):
+        token = tokenlist[0]
         if token.tokenval == TokenVal.TIMES.value or token.tokenval == TokenVal.DIVISION.value:
             return True
         else:
             return False
 
 
-    def isRelationalOperator(self, token):
+    def isRelationalOperator(self, tokenlist):
+        token = tokenlist[0]
         if token.tokenval == TokenVal.LOGIC_EQUALS.value or token.tokenval == TokenVal.HIGHER.value or
         token.tokenval == TokenVal.LESS.value or token.tokenval == TokenVal.LESS_EQUALS.value or
         token.tokenval == TokenVal.HIGHER_EQUALS.value:
@@ -39,9 +41,46 @@ class syntax_analyzer(object):
             return False
 
 
-    def isType(self, token):
+    def isType(self, tokenlist):
+        token = tokenlist[0]
         if  token.tokenval == TokenVal.FLOAT_TYPE.value or
         token.tokenval == TokenVal.INTEGER_TYPE.value:
             return True
         else:
             return False
+
+
+    def isUnaryExpression(self, tokenlist):
+        if self.isFactor(tokenlist):
+            return True
+        elif self.isSumOperator(tokenlist) and self.isFactor(tokenlist[1:]):
+            return True
+        else:
+            return False
+
+            
+    def isFactor(self, tokenlist):
+
+        if tokenlist[0].tokenval == TokenVal.OPEN_PARENTHESES.value and
+        self.isExpression(tokenlist[1:]) and tokenlist[2].tokenval == TokenVal.CLOSE_PARENTHESES.value:
+            return True
+        elif self.isVar(tokenlist):
+            return True
+        elif self.isCallFunction(tokenlist):
+            returnt True
+        elif self.isNumber(tokenlist):
+            return True
+        else:
+            return False
+
+
+    def isExpression(self, tokenlist):
+        pass
+
+
+    def isVar(self, tokenlist):
+        pass
+
+
+    def isCallFunction(self, tokenlist):
+        pass
