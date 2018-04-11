dccc5a7b41f462473d52e2a43dcb5c46b4b0bf4d
Paulo Batista <pauloc@linuxmail.org>
2018-04-11 16:37:12 -0300

    function to recognize math operators #10

--- syntax/Syntax.py
+++ syntax/Syntax.py
@@ -1,4 +1,4 @@
-from lexical.structure.token.TokenVal import Tokenval
+from lexical.structure.token.TokenVal import TokenVal
 class syntax_analyzer(object):
 
     def __init__(self):
@@ -6,9 +6,23 @@ class syntax_analyzer(object):
 
 
     def isNumber(self, token):
-        if token.tokenval == Tokenval.INTEGER_NUMBER or
-        token.tokenval == Tokenval.FLOAT_NUMBER or
-        token.tokenval == Tokenval.SCIENTIFIC_NOTATION:
+        if token.tokenval == TokenVal.INTEGER_NUMBER.value or
+        token.tokenval == TokenVal.FLOAT_NUMBER.value or
+        token.tokenval == TokenVal.SCIENTIFIC_NOTATION.value:
+            return True
+        else:
+            return False
+
+
+    def isSumOperator(self, token):
+        if token.tokenval == TokenVal.PLUS.value or token.tokenval == TokenVal.MINUS.value:
+            return True
+        else:
+            return False
+
+
+    def isMultiOperator(self, token):
+        if token.tokenval == TokenVal.TIMES.value or token.tokenval == TokenVal.DIVISION.value:
             return True
         else:
             return False
